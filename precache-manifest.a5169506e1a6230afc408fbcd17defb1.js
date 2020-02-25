519499ddfc5eef6dbfe89b2de370b7cbfe372de5
Eric Amrhein <eamrhein@protonmail.ch>
2020-02-25 12:39:45 -0800

    Updates

---
+++ precache-manifest.a5169506e1a6230afc408fbcd17defb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1efdcc91d88a5cdb68ec815c8d8c4da5",
+    "url": "/event_tool/index.html"
+  },
+  {
+    "revision": "ac70306eab454543a1cb",
+    "url": "/event_tool/static/css/2.54084663.chunk.css"
+  },
+  {
+    "revision": "604b3529ba00b4d13d3d",
+    "url": "/event_tool/static/css/main.4177a08e.chunk.css"
+  },
+  {
+    "revision": "ac70306eab454543a1cb",
+    "url": "/event_tool/static/js/2.763b9f06.chunk.js"
+  },
+  {
+    "revision": "22f6ec7ef22eb3f84567a8bc5a14549e",
+    "url": "/event_tool/static/js/2.763b9f06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "604b3529ba00b4d13d3d",
+    "url": "/event_tool/static/js/main.2159bbbe.chunk.js"
+  },
+  {
+    "revision": "f4ba992a08e5068dec00",
+    "url": "/event_tool/static/js/runtime-main.8202628e.js"
+  }
+]);
